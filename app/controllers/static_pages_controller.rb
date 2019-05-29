6d0716981a6f17b11517ec5046473ce8fda2e90a
KingChristy <king.christy@gmail.com>
2019-05-29 19:22:53 +0000

    Finish User Sign Up

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,14 +10,4 @@ class StaticPagesController < ApplicationController
   
   def fun
   end
-  
-  #add the code to the else part
-  def search  
-    if params[:search].blank?  
-      redirect_to(root_path, alert: "Empty field!") and return  
-    else  
-      @parameter = params[:search].downcase  
-      @results = Store.all.where("lower(name) LIKE :search", search: @parameter)  
-    end 
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get 'users/new'
   get 'static_pages/home'
   get 'static_pages/jobs'
   get 'static_pages/living'
   get 'static_pages/fun'
+  get 'static_pages/search'
+  get '/signup', to: 'users#new' 
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'static_pages#home'
+
+resources :users 
+
 end
